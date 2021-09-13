4e6056e0333ed6d73e818d25c9b96d694d752a40
JaejuGom <inkcloud365@gmail.com>
2021-09-13 16:41:57 +0900

    테스트3

--- src/main/java/org/tikim/boot/BootApplication.java
+++ src/main/java/org/tikim/boot/BootApplication.java
@@ -9,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class BootApplication {
+    
     public static void main(String[] args) {
         SpringApplication.run(BootApplication.class, args);
     }
